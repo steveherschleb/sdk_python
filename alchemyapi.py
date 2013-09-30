90916c0310f9585dbdb160af84672cb534d11ac9
Steve Herschleb <sherschleb@alchemyapi.com>
2013-09-30 15:59:58 -0600

    update comments

--- alchemyapi.py
+++ alchemyapi.py
@@ -108,6 +108,12 @@ def setkey(key):
 	Writes the API key to api_key.txt file. It will create the file if it doesn't exist.
 	This function is intended to be called from the Python command line using: python -c 'import alchemyapi;alchemyapi.setkey("API_KEY");'
 	If you don't have an API key yet, register for one at: http://www.alchemyapi.com/api/register.html
+	
+	INPUT:
+	key -> Your API key from AlchemyAPI. Should be 40 hex characters
+	
+	OUTPUT:
+	none
 	"""
 
 	#write the key to the file
@@ -117,14 +123,22 @@ def setkey(key):
 
 
 
-def sentiment(flavor,  data, options={}):
+def sentiment(flavor, data, options={}):
 	"""
 	Calculates the sentiment for text, a URL or HTML.
 	For an overview, please refer to: http://www.alchemyapi.com/products/features/sentiment-analysis/
 	For the docs, please refer to: http://www.alchemyapi.com/api/sentiment-analysis/
 	
+	INPUT:
+	flavor -> which version of the call, i.e. text, url or html.
+	data -> the data to analyze, either the text, the url or html code.
+	options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
+	
 	Available Options:
-	showSourceText	-> 0: disabled, 1: enabled
+	showSourceText -> 0: disabled (default), 1: enabled
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
 	"""
 
 	if API_KEY == '':
@@ -152,8 +166,17 @@ def sentiment_targeted(flavor, data, target, options={}):
 	For an overview, please refer to: http://www.alchemyapi.com/products/features/sentiment-analysis/
 	For the docs, please refer to: http://www.alchemyapi.com/api/sentiment-analysis/
 	
+	INPUT:
+	flavor -> which version of the call, i.e. text, url or html.
+	data -> the data to analyze, either the text, the url or html code.
+	target -> the word or phrase to run sentiment analysis on.
+	options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
+	
 	Available Options:
 	showSourceText	-> 0: disabled, 1: enabled
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
 	"""
 
 	if API_KEY == '':
@@ -184,6 +207,17 @@ def author(flavor, data, options={}):
 	Extracts the author from a URL or HTML.
 	For an overview, please refer to: http://www.alchemyapi.com/products/features/author-extraction/
 	For the docs, please refer to: http://www.alchemyapi.com/api/author-extraction/
+	
+	INPUT:
+	flavor -> which version of the call, i.e. text, url or html.
+	data -> the data to analyze, either the text, the url or html code.
+	options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
+
+	Availble Options:
+	none
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
 	"""
 	
 	if API_KEY == '':
@@ -210,12 +244,20 @@ def keywords(flavor, data, options={}):
 	Extracts the keywords from text, a URL or HTML.
 	For an overview, please refer to: http://www.alchemyapi.com/products/features/keyword-extraction/
 	For the docs, please refer to: http://www.alchemyapi.com/api/keyword-extraction/
+	
+	INPUT:
+	flavor -> which version of the call, i.e. text, url or html.
+	data -> the data to analyze, either the text, the url or html code.
+	options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
 			
 	Available Options:
-	keywordExtractMode
-	sentiment
-	showSourceText
-	maxRetrieve
+	keywordExtractMode -> normal (default), strict
+	sentiment -> analyze sentiment for each keyword. 0: disabled (default), 1: enabled. Requires 1 additional API transaction if enabled.
+	showSourceText -> 0: disabled (default), 1: enabled.
+	maxRetrieve -> the max number of keywords returned (default: 50)
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
 	"""
 	
 	if API_KEY == '':
@@ -243,9 +285,12 @@ def concepts(flavor, data, options={}):
 	For the docs, please refer to: http://www.alchemyapi.com/api/concept-tagging/ 
 	
 	Available Options:
-	maxRetrieve
-	linkedData
-	showSourceText
+	maxRetrieve -> the maximum number of concepts to retrieve (default: 8)
+	linkedData -> include linked data, 0: disabled, 1: enabled (default)
+	showSourceText -> 0:disabled (default), 1: enabled
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
 	"""
 
 	if API_KEY == '':
@@ -272,13 +317,22 @@ def entities(flavor, data, options={}):
 	For an overview, please refer to: http://www.alchemyapi.com/products/features/entity-extraction/ 
 	For the docs, please refer to: http://www.alchemyapi.com/api/entity-extraction/
 	
-	disambiguate 
-	linkedData
-	coreference
-	quotations 
-	sentiment
-	showSourceText
-	maxRetrieve
+	INPUT:
+	flavor -> which version of the call, i.e. text, url or html.
+	data -> the data to analyze, either the text, the url or html code.
+	options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
+	
+	Available Options:
+	disambiguate -> disambiguate entities (i.e. Apple the company vs. apple the fruit). 0: disabled, 1: enabled (default)
+	linkedData -> include linked data on disambiguated entities. 0: disabled, 1: enabled (default) 
+	coreference -> resolve coreferences (i.e. the pronouns that correspond to named entities). 0: disabled, 1: enabled (default)
+	quotations -> extract quotations by entities. 0: disabled (default), 1: enabled.
+	sentiment -> analyze sentiment for each entity. 0: disabled (default), 1: enabled. Requires 1 additional API transction if enabled.
+	showSourceText -> 0: disabled (default), 1: enabled 
+	maxRetrieve -> the maximum number of entities to retrieve (default: 50)
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
 	"""
 	
 	if API_KEY == '':
@@ -305,8 +359,16 @@ def category(flavor, data, options={}):
 	For an overview, please refer to: http://www.alchemyapi.com/products/features/text-categorization/
 	For the docs, please refer to: http://www.alchemyapi.com/api/text-categorization/
 	
+	INPUT:
+	flavor -> which version of the call, i.e. text, url or html.
+	data -> the data to analyze, either the text, the url or html code.
+	options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
+	
 	Available Options:
-	showSourceText
+	showSourceText -> 0: disabled (default), 1: enabled
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
 	"""
 	
 	if API_KEY == '':
@@ -333,17 +395,25 @@ def relations(flavor, data, options={}):
 	For an overview, please refer to: http://www.alchemyapi.com/products/features/relation-extraction/ 
 	For the docs, please refer to: http://www.alchemyapi.com/api/relation-extraction/
 	
+	INPUT:
+	flavor -> which version of the call, i.e. text, url or html.
+	data -> the data to analyze, either the text, the url or html code.
+	options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
+	
 	Available Options:
-	'&sentiment=0' +
-	'&keywords=0' +
-	'&entities=0' +
-	'&requireEntities=0' +
-	'&sentimentExcludeEntities=1' +
-	'&disambiguate=1' +
-	'&linkedData=1' +
-	'&coreference=1' + 
-	'&showSourceText=0' +
-	'&maxRetrieve=50')
+	sentiment -> 0: disabled (default), 1: enabled. Requires one additional API transaction if enabled.
+	keywords -> extract keywords from the subject and object. 0: disabled (default), 1: enabled. Requires one additional API transaction if enabled.
+	entities -> extract entities from the subject and object. 0: disabled (default), 1: enabled. Requires one additional API transaction if enabled.
+	requireEntities -> only extract relations that have entities. 0: disabled (default), 1: enabled.
+	sentimentExcludeEntities -> exclude full entity name in sentiment analysis. 0: disabled, 1: enabled (default)
+	disambiguate -> disambiguate entities (i.e. Apple the company vs. apple the fruit). 0: disabled, 1: enabled (default)
+	linkedData -> include linked data with disambiguated entities. 0: disabled, 1: enabled (default).
+	coreference -> resolve entity coreferences. 0: disabled, 1: enabled (default)  
+	showSourceText -> 0: disabled (default), 1: enabled.
+	maxRetrieve -> the maximum number of relations to extract (default: 50, max: 100)
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
 	"""
 	
 	if API_KEY == '':
@@ -369,6 +439,17 @@ def language(flavor, data, options={}):
 	Detects the language for text, a URL or HTML.
 	For an overview, please refer to: http://www.alchemyapi.com/api/language-detection/ 
 	For the docs, please refer to: http://www.alchemyapi.com/products/features/language-detection/
+	
+	INPUT:
+	flavor -> which version of the call, i.e. text, url or html.
+	data -> the data to analyze, either the text, the url or html code.
+	options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
+
+	Available Options:
+	none
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
 	"""
 	
 	if API_KEY == '':
@@ -395,9 +476,17 @@ def text_clean(flavor, data, options={}):
 	For an overview, please refer to: http://www.alchemyapi.com/products/features/text-extraction/
 	For the docs, please refer to: http://www.alchemyapi.com/api/text-extraction/
 	
+	INPUT:
+	flavor -> which version of the call, i.e. text, url or html.
+	data -> the data to analyze, either the text, the url or html code.
+	options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
+	
 	Available Options:
-	'&useMetadata=1' +
-	'&extractLinks=0')
+	useMetadata -> utilize meta description data, 0: disabled, 1: enabled (default)
+	extractLinks -> include links, 0: disabled (default), 1: enabled.
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
 	"""
 	
 	if API_KEY == '':
@@ -423,6 +512,17 @@ def text_raw(flavor, data, options={}):
 	Extracts the raw text (includes ads, navigation, etc.) for a URL or HTML.
 	For an overview, please refer to: http://www.alchemyapi.com/products/features/text-extraction/ 
 	For the docs, please refer to: http://www.alchemyapi.com/api/text-extraction/
+	
+	INPUT:
+	flavor -> which version of the call, i.e. text, url or html.
+	data -> the data to analyze, either the text, the url or html code.
+	options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
+	
+	Available Options:
+	none
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
 	"""
 	
 	if API_KEY == '':
@@ -450,8 +550,16 @@ def text_title(flavor, data, options={}):
 	For an overview, please refer to: http://www.alchemyapi.com/products/features/text-extraction/ 
 	For the docs, please refer to: http://www.alchemyapi.com/api/text-extraction/
 	
+	INPUT:
+	flavor -> which version of the call, i.e. text, url or html.
+	data -> the data to analyze, either the text, the url or html code.
+	options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
+	
 	Available Options:
-	'&useMetadata=1')
+	useMetadata -> utilize title info embedded in meta data, 0: disabled, 1: enabled (default) 
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
 	"""
 	
 	if API_KEY == '':
@@ -478,6 +586,17 @@ def microformats(flavor, data, options={}):
 	Parses the microformats for a URL or HTML.
 	For an overview, please refer to: http://www.alchemyapi.com/products/features/microformats-parsing/
 	For the docs, please refer to: http://www.alchemyapi.com/api/microformats-parsing/
+	
+	INPUT:
+	flavor -> which version of the call, i.e.  url or html.
+	data -> the data to analyze, either the the url or html code.
+	options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
+	
+	Available Options:
+	none
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
 	"""
 	
 	if API_KEY == '':
@@ -504,6 +623,17 @@ def feeds(flavor, data, options={}):
 	Detects the RSS/ATOM feeds for a URL or HTML.
 	For an overview, please refer to: http://www.alchemyapi.com/products/features/feed-detection/ 
 	For the docs, please refer to: http://www.alchemyapi.com/api/feed-detection/
+	
+	INPUT:
+	flavor -> which version of the call, i.e.  url or html.
+	data -> the data to analyze, either the the url or html code.
+	options -> various parameters that can be used to adjust how the API works, see below for more info on the available options.
+
+	Available Options:
+	none
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
 	"""
 	
 	if API_KEY == '':
@@ -526,6 +656,17 @@ def feeds(flavor, data, options={}):
 
 
 def analyze(url):
+	"""
+	HTTP Request wrapper that is called by the endpoint functions. This function is not intended to be called through an external interface. 
+	It makes the call, then converts the returned JSON string into a Python object. 
+	
+	INPUT:
+	url -> the full URI encoded url
+
+	OUTPUT:
+	The response, already converted from JSON to a Python object. 
+	"""
+
 	try:
 		#build the request with uri encoding
 		req = urllib2.Request(url)
